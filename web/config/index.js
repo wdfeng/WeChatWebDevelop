2b1963e7bfd91d77133e7b90a79e303c1c18a83c
wdfeng <wdfeng@gmail.com>
2021-08-30 09:37:46 +0800

    Update index.js

--- web/config/index.js
+++ web/config/index.js
@@ -5,14 +5,4 @@ module.exports = {
 	appID:"wx12287113c07d18c5",
 	appsecret:"9257b3bd553d3d80fb2ba5be95ab88f2",
 
-	//好酷啊
-	// token:"Intel",
-	// appID:"wx8307f182928d551b",
-	// appsecret:"ac78caf959a77c550cd674acafe75dce",
-
-	//Partner Test
-	// token:"Xingdong2Intel",
-	// appID:"wxebc9f0331840bbb4",
-	// appsecret:"94de7fbad28493e6417230d46040d975",
-
 }
